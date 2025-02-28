40c67ef7432e504bebd06550c0cb1b4f4701c3a6
Manuel <ManuelRGlod18@gmail.com>
2025-02-28 12:21:04 -0400

    aplicando tabla y arreglando errores

--- cmd/api/db.go
+++ cmd/api/db.go
@@ -8,8 +8,24 @@ import (
 
 var DB *sql.DB
 
-func openDB() error {
+func setupDB() error {
+	_, err := DB.Exec(`
+		CREATE TABLE IF NOT EXISTS medicamentos(
+			id INTEGER NOT NULL PRIMARY KEY,
+			nombre TEXT,
+			componenteprincipal TEXT,
+			precio TEXT
+		)
+	`)
 
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func openDB() error {
 	db, err := sql.Open("sqlite3", "./sqlite3.db")
 	if err != nil {
 		return err
@@ -21,23 +37,4 @@ func openDB() error {
 
 func closeDB() error {
 	return DB.Close()
-}
-
-func setupDB() error {
-
-	_, err := DB.Exec(`
-		CREATE TABLE IF NOT EXISTS medicamentos(
-			id not null PRIMARY KEY,
-			nombre TEXT,
-			componenteprincipal TEXT,
-			precio TEXT
-	`)
-
-	if err != nil {
-		return err
-
-	}
-
-	return nil
-
 }
