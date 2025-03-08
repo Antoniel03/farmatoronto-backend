64ef6347ba6ac52bf7191996fea16f022d33590d
Manuel <ManuelRGlod18@gmail.com>
2025-03-08 19:21:06 -0400

    modificacion base de datos inicializador

--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -2,19 +2,19 @@ CREATE TABLE IF NOT EXISTS usuarios(
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             correo VARCHAR(50) UNIQUE,
             contrasena VARCHAR(50),
-      tipo_usuario VARCHAR(15)
-      codempleado INTEGER,
-        FOREIGN KEY(codempleado) REFERENCES empleados(id)
+            codempleado INTEGER,
+            FOREIGN KEY(codempleado) REFERENCES empleados(id)
       );
 
 CREATE TABLE IF NOT EXISTS empleados(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,-- id puede ser cedula o pasaporte
     nombre VARCHAR(50),
     apellido VARCHAR(50),
+    cargo VARCHAR(50),
     fecha_nacimiento VARCHAR(50),
     direccion VARCHAR(100),
-    telefono VARCHAR(12),
-    email VARCHAR(50) UNIQUE
+    telefono VARCHAR(12)UNIQUE,
+    cedula VARCHAR(12)UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS administrador (
@@ -40,9 +40,7 @@ CREATE TABLE IF NOT EXISTS pasantes (
 
 CREATE TABLE IF NOT EXISTS farmaceutico (
     id INTEGER NOT NULL PRIMARY KEY,
-    codfarmacia INTEGER,
-    FOREIGN KEY (id) REFERENCES empleados(id),
-    FOREIGN KEY (codfarmacia) REFERENCES farmacia_sucursal(id)
+    FOREIGN KEY (id) REFERENCES empleados(id)
 );
 
 
@@ -50,17 +48,25 @@ CREATE TABLE IF NOT EXISTS medicamentos(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     nombre VARCHAR(50),
     componenteprincipal VARCHAR(50),
-    accion VARCHAR(50),
+   accion_terapeutica_id INTEGER,
     presentacion VARCHAR(50),
-    precio REAL
+    precio REAL,
+    FOREIGN KEY(accion_terapeutica_id) REFERENCES accion_terapeutica(id)
+);
+
+CREATE TABLE IF NOT EXISTS accion_terapeutica(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(50)
+    descripcion VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS monodrogas(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nombre VARCHAR(50)
+    nombre VARCHAR(50),
+    descripcion VARCHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS Medic_monodrogas(
+CREATE TABLE IF NOT EXISTS medic_monodrogas(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     codmedicamento INTEGER,
     codmonodroga INTEGER,
@@ -69,7 +75,7 @@ CREATE TABLE IF NOT EXISTS Medic_monodrogas(
 );
 
 CREATE TABLE IF NOT EXISTS stock(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
     farmacia_id INTEGER,
     medicamento_id INTEGER,
     cantidad INTEGER,
@@ -83,6 +89,7 @@ CREATE TABLE IF NOT EXISTS rotacion (
     sucursal_id INTEGER,
     fecha_inicio DATE,
     fecha_final DATE,
+    observaciones VARCHAR(100),
     FOREIGN KEY(empleado_id) REFERENCES empleados(id),
     FOREIGN KEY(sucursal_id) REFERENCES farmacia_sucursal(id)
 );
@@ -90,14 +97,14 @@ CREATE TABLE IF NOT EXISTS rotacion (
 CREATE TABLE IF NOT EXISTS farmacia_sucursal(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     ciudad_id INTEGER,
-    nombre VARCHAR(50),
-    direccion VARCHAR(100),
+    direccion VARCHAR(100)UNIQUE,
+    telefono VARCHAR(12)UNIQUE,
     FOREIGN KEY(ciudad_id) REFERENCES ciudad(id)
 );
 
 CREATE TABLE IF NOT EXISTS ciudad(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50)UNIQUE,
     estado VARCHAR(50)
 );
 
@@ -120,22 +127,24 @@ CREATE TABLE IF NOT EXISTS compra_medicamentos(
 
 CREATE TABLE IF NOT EXISTS ordencompra(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    codfarmacia INTEGER,
+    codfarmacia INTEGER, 
     codanalista INTEGER,
+    codlaboratorio INTEGER,
     fechaEmision DATE,
     formaPago VARCHAR(50),
     FOREIGN KEY(codfarmacia) REFERENCES farmacia_sucursal(id),
-    FOREIGN KEY(codanalista) REFERENCES analista(id)
+    FOREIGN KEY(codanalista) REFERENCES analista(id),
+    FOREIGN KEY(codlaboratorio) REFERENCES laboratorio(id)
 );
 
 CREATE TABLE IF NOT EXISTS laboratorio(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     nombre VARCHAR(50),
-    direccion VARCHAR(100),
-    telefono VARCHAR(12)
+    direccion VARCHAR(100)UNIQUE,
+    telefono VARCHAR(12) UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS Medic_labs(
+CREATE TABLE IF NOT EXISTS medic_labs(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     codmedicamento INTEGER,
     codlaboratorio INTEGER,
